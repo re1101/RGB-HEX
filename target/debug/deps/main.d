9c5c56950b7c4f463e0d0cf956118955a7a42f90
re1101 <romaro1101@gmail.com>
2024-11-11 14:02:17 -0600

    Corrected build errors
    Known Bugs: stays in R loop

---
+++ target/debug/deps/main.d
@@ -0,0 +1,5 @@
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\main.exe: src/main
+
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\main.d: src/main
+
+src/main:
---
+++ target/debug/main.d
@@ -0,0 +1,1 @@
+C:\Users\GPOSMPC19\OneDrive\ -\ Resistencias\ San\ Marino,\ S.A.\ de\ C.V\Documentos\Varios\RGB-HEX\target\debug\main.exe: C:\Users\GPOSMPC19\OneDrive\ -\ Resistencias\ San\ Marino,\ S.A.\ de\ C.V\Documentos\Varios\RGB-HEX\src\main
