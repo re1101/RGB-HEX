6011a55482ff6c2014c0ef1649ddb549d307403e
re1101 <romaro1101@gmail.com>
2024-11-12 09:52:44 -0600

    Final details
    Correct format in prints.
    cleared inputs after being evaluated or asigned.
    Correct chars being pushed to bHex.

---
+++ target/debug/deps/main-aaf1ca6a0ffc65a4.d
@@ -0,0 +1,5 @@
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\libmain-aaf1ca6a0ffc65a4.rmeta: src/main
+
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\main-aaf1ca6a0ffc65a4.d: src/main
+
+src/main:
