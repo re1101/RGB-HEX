c902723f43e4984e2bf69d62db76530d3804d4e2
re1101 <romaro1101@gmail.com>
2024-11-15 13:18:50 -0600

    Main extention to .rs
    main had no extention leading to some programs reognizing it as MakeFile

---
+++ target/debug/deps/RGB_HEX-1db8229909cd88d1.d
@@ -0,0 +1,5 @@
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\libRGB_HEX-1db8229909cd88d1.rmeta: src/main.rs
+
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\RGB_HEX-1db8229909cd88d1.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/RGB_HEX-2836ac870fbd25f0.d
@@ -0,0 +1,5 @@
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\libRGB_HEX-2836ac870fbd25f0.rmeta: src/main.rs
+
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\RGB_HEX-2836ac870fbd25f0.d: src/main.rs
+
+src/main.rs:
