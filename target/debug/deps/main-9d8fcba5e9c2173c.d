a229ea80a50e3a723ebc45c6de0355ed51449f02
re1101 <romaro1101@gmail.com>
2024-11-26 11:50:19 -0600

    Unknown MetaChanges

---
+++ target/debug/deps/main-9d8fcba5e9c2173c.d
@@ -0,0 +1,5 @@
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\main-9d8fcba5e9c2173c.exe: src/main.rs
+
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\main-9d8fcba5e9c2173c.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/main-e8e282b7d06d0c7f.d
@@ -0,0 +1,5 @@
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\libmain-e8e282b7d06d0c7f.rmeta: src/main.rs
+
+C:\Users\GPOSMPC19\OneDrive - Resistencias San Marino, S.A. de C.V\Documentos\Varios\RGB-HEX\target\debug\deps\main-e8e282b7d06d0c7f.d: src/main.rs
+
+src/main.rs:
